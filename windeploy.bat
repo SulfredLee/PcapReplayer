a16616333e54ece2efeee428c299ed741cb64540
Sulfred Lee <sflee1112@gmail.com>
2018-06-10 22:07:15 +0800

    Update script and make file for windows version

--- windeploy.bat
+++ windeploy.bat
@@ -1,2 +1,2 @@
-:: C:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\windeployqt.exe .\build\BackEnd\Debug\PcapReplayer.exe
-C:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\windeployqt.exe .\build\BackEnd\Release\PcapReplayer.exe
+:: C:\Qt\5.6.2\5.6\msvc2013_64\bin\windeployqt.exe .\build\BackEnd\Debug\PcapReplayer.exe
+C:\Qt\5.6.2\5.6\msvc2013_64\bin\windeployqt.exe .\build\BackEnd\Release\PcapReplayer.exe
